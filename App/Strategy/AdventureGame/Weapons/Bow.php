989b6d7a726b9c0c8a453ede4babada23775d222
Vlad Ogir <vlad@homesearch.co.uk>
2020-07-26 21:19:29 +0100

    Removed code for ammunition since it's out of scope for what I am trying to accomplish

--- App/Strategy/AdventureGame/Weapons/Bow.php
+++ App/Strategy/AdventureGame/Weapons/Bow.php
@@ -2,20 +2,10 @@
 
 namespace App\Strategy\AdventureGame\Weapons;
 
-use App\Strategy\AdventureGame\Weapons\Traits\Ammunition;
-
 class Bow extends BaseWeapon
 {
-    use Ammunition; 
-
     public function useWeapon(): string
     {
-        if (!$this->hasAmmunition()) {
-            return 'we run out of arrows';
-        }
-
-        $this->removeAmmunition(1);
-
         return 'shooting an arrow with a bow';
     }
 }
